13d67a7b8f261e7ed0d5652a0aa382941234568d
Bart63 <bartek.durys@gmail.com>
2021-10-10 16:44:04 +0200

    Korekcja znakow bialych

--- zadanie2/scripts.js
+++ zadanie2/scripts.js
@@ -1,15 +1,13 @@
 "use strict"
 
-
-
 let todoList = [];
 
 let initList = function() {
-
     let savedList = window.localStorage.getItem("todos");
-    if (savedList != null)
+    if (savedList != null) {
         todoList = JSON.parse(savedList);
-    else{
+    }
+    else {
         todoList.push(
             {
                 title: "Learn JS",
@@ -36,15 +34,14 @@ $.ajax({
     },
     success: (data) => {
       //console.log(data);
-
       todoList = data;
     },
     error: (err) => {
       console.log(err.responseJSON);
     }
-   });
+});
    
-   let deleteTodo = function(index) {
+let deleteTodo = function(index) {
     todoList.splice(index,1);
     updateJSONbin();
 }
@@ -68,93 +65,81 @@ let updateJSONbin = function() {
 
 //initList();
 
-
-
-
 let updateTodoList = function() {
-
     let todoTable = $('#todoTable').get(0);
-
     
     while (todoTable.rows.length != 1){
         todoTable.deleteRow(todoTable.rows.length - 1);
     }
 
-    /*
-
-    let todoListDiv =
+    /*let todoListDiv =
     document.getElementById("todoListView");
 
     //remove all elements
     while (todoListDiv.firstChild) {
         todoListDiv.removeChild(todoListDiv.firstChild);
-    }
-*/
+    }*/
+    
     //add all elements
     let filterInput = $("#inputSearch").get(0);  
     let startFilterDate = $("#fromInputDateSearch").get(0); 
     let endFilterDate = $("#toInputDateSearch").get(0); 
     for (let todo in todoList) {
-
         if (
             (filterInput.value == "") ||
             (todoList[todo].title.includes(filterInput.value)) ||
             (todoList[todo].description.includes(filterInput.value))
           ) {
-                if ((!startFilterDate.value && !endFilterDate.value) ||
-                (todoList[todo].dueDate >= startFilterDate.value && !endFilterDate.value) ||
-                (!startFilterDate.value && endFilterDate.value >= todoList[todo].dueDate) ||
-                (todoList[todo].dueDate >= startFilterDate.value && todoList[todo].dueDate <= endFilterDate.value)){
-
-                    
+            if ((!startFilterDate.value && !endFilterDate.value) ||
+            (todoList[todo].dueDate >= startFilterDate.value && !endFilterDate.value) ||
+            (!startFilterDate.value && endFilterDate.value >= todoList[todo].dueDate) ||
+            (todoList[todo].dueDate >= startFilterDate.value && todoList[todo].dueDate <= endFilterDate.value)){
+                
                 /*
-                    let newDeleteButton = document.createElement("input");
-                    newDeleteButton.type = "button";
-                    newDeleteButton.value = "x";
-                    newDeleteButton.addEventListener("click",
-                        function() {
-                            deleteTodo(todo);
-                        });
-
-                    let newElement = document.createElement("div");
-                    let newContent = document.createTextNode(
-                        todoList[todo].title + " " + todoList[todo].description);
-                    newElement.appendChild(newContent);
-                    newElement.appendChild(newDeleteButton);
-                    todoListDiv.appendChild(newElement);
-                    */
-
-                    
-
-                    let row = $('<tr></tr>')
-                    .addClass("table-primary")
-                    .append($('<td></td>')
-                    .append($('<text></text>')
-                    .text(todoList[todo].title)))
-                    .append($('<td></td>')
-                    .append($('<text></text>')
-                    .text(todoList[todo].description)))
-                    .append($('<td></td>')
-                    .append($('<text></text>')
-                    .text(todoList[todo].place)))
-                    .append($('<td></td>')
-                    .append($('<text></text>')
-                    .text(todoList[todo].dueDate)))
-                    .append($('<td></td>')
-                    .append($('<input></input>')
-                    .attr({type: "button", value: "delete"})
-                    .addClass("btn btn-outline-danger")
-                    .on("click",
+                let newDeleteButton = document.createElement("input");
+                newDeleteButton.type = "button";
+                newDeleteButton.value = "x";
+                newDeleteButton.addEventListener("click",
                     function() {
                         deleteTodo(todo);
-                    })
-                    ));
+                    });
 
+                let newElement = document.createElement("div");
+                let newContent = document.createTextNode(
+                    todoList[todo].title + " " + todoList[todo].description);
+                newElement.appendChild(newContent);
+                newElement.appendChild(newDeleteButton);
+                todoListDiv.appendChild(newElement);
+                */
 
-                    $("#todoTable")
-                    .append(row);
-                }
-          }
+                let row = $('<tr></tr>')
+                .addClass("table-primary")
+                .append($('<td></td>')
+                .append($('<text></text>')
+                .text(todoList[todo].title)))
+                .append($('<td></td>')
+                .append($('<text></text>')
+                .text(todoList[todo].description)))
+                .append($('<td></td>')
+                .append($('<text></text>')
+                .text(todoList[todo].place)))
+                .append($('<td></td>')
+                .append($('<text></text>')
+                .text(todoList[todo].dueDate)))
+                .append($('<td></td>')
+                .append($('<input></input>')
+                .attr({type: "button", value: "delete"})
+                .addClass("btn btn-outline-danger")
+                .on("click",
+                function() {
+                    deleteTodo(todo);
+                })
+                ));
+                
+                $("#todoTable")
+                .append(row);
+            }
+        }
     }
 }
 
@@ -162,27 +147,24 @@ setInterval(updateTodoList, 1000);
 
 let addTodo = function() {
     //get the elements in the form
-      let inputTitle = $("#inputTitle").get(0);
-      let inputDescription = $("#inputDescription").get(0);
-      let inputPlace = $("#inputPlace").get(0);
-      let inputDate = $("#inputDate").get(0);
+    let inputTitle = $("#inputTitle").get(0);
+    let inputDescription = $("#inputDescription").get(0);
+    let inputPlace = $("#inputPlace").get(0);
+    let inputDate = $("#inputDate").get(0);
     //get the values from the form
-      let newTitle = inputTitle.value;
-      let newDescription = inputDescription.value;
-      let newPlace = inputPlace.value;
-      let newDate = new Date(inputDate.value);
+    let newTitle = inputTitle.value;
+    let newDescription = inputDescription.value;
+    let newPlace = inputPlace.value;
+    let newDate = new Date(inputDate.value);
     //create new item
-      let newTodo = {
-          dueDate: newDate,
-          place: newPlace,
-          description: newDescription,
-          title: newTitle
-          
-      };
+    let newTodo = {
+        dueDate: newDate,
+        place: newPlace,
+        description: newDescription,
+        title: newTitle
+    };
     //add item to the list
-      todoList.push(newTodo);
-
-      //window.localStorage.setItem("todos", JSON.stringify(todoList));
-
-      updateJSONbin();
-  }
+    todoList.push(newTodo);
+    //window.localStorage.setItem("todos", JSON.stringify(todoList));
+    updateJSONbin();
+}
