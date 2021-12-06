c071a556373c0e4a24581ffdaaca7c157c87e8ce
Bart63 <bartek.durys@gmail.com>
2021-12-06 05:00:38 +0100

    Add cors restrictions

--- zadanie4/server/index.js
+++ zadanie4/server/index.js
@@ -6,7 +6,9 @@ const app = express();
 const { router } = require('./router');
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:8080'
+}));
 
 router(app);
 app.listen(5000);
